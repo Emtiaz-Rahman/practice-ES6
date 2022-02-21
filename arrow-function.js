f0fd1674ab3fa5d373972926985ded38d732b793
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-21 15:51:50 +0600

    ex-2 complete

---
+++ arrow-function.js
@@ -0,0 +1,11 @@
+// ex=3.1
+
+const multyply = num => num / 5;
+const output = multyply(15);
+console.log(output);
+
+// ex=3.2
+const calculation=(x,y)=>((x+2)*(y+2));
+const result=calculation(4,2);
+console.log(result);
+
