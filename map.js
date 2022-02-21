9474244b13aea0afd07b9f88993a77f45856fd7d
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-21 19:36:07 +0600

    ex-5,6 done

---
+++ map.js
@@ -0,0 +1,10 @@
+// ex=5
+const numbers = [4, 6, 8, 10];
+const multyply = numbers.map(x => x * 5);
+console.log(multyply);
+
+
+// ex=6
+const numbers=[1,2,3,4,5,6,7,8,9]
+const number=numbers.filter(x=>x%2==0);
+console.log(number);
