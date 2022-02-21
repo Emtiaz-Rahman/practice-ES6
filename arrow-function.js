17c72fe6ca12f4890060fb9c1486cbccb623b0f8
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-21 19:12:33 +0600

    ex-3.4

--- arrow-function.js
+++ arrow-function.js
@@ -1,4 +1,4 @@
-// ex=3.1
+ex=3.1
 
 const multyply = num => num / 5;
 const output = multyply(15);
@@ -13,4 +13,15 @@ console.log(result);
 
 const calculation = (x, y, z) => x * y * z;
 const result = calculation(3, 3, 3);
+console.log(result);
+
+// ex=3.4
+
+const calculation = (x, y) => {
+    const sum1 = x + 2;
+    const sum2 = y + 2;
+    const multyply = sum1 * sum2;
+    return multyply;
+}
+const result = calculation(4, 2);
 console.log(result);
